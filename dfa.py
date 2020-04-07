c8c4b4dd052de9b23e669a67e26bdea6ba7dd609
Mirto <32554413+MyrtoP@users.noreply.github.com>
2020-04-07 21:45:18 +0300

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -6,25 +6,23 @@
 στα σημεία (Α), (Β) και (Γ) - διαβάστε τα σχόλια!
 """
 
-
-transitions = { 
-
-	# (Α) Συμπληρώστε τον πίνακα μεταβάσεων ως λεξικό (dictionary).
+# (Α) Συμπληρώστε τον πίνακα μεταβάσεων ως λεξικό (dictionary).
 	# Η αρχική κατάσταση πρέπει να ονομάζεται 's0'.
 	# Για λεπτομέρειες δείτε στο:
 	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id7
+# anagnorizoume ari8mous tis morfis MONO   123.56 ,  123. ,   .56789
+transitions = { 's0':{'DIGIT':'s1','.':'s2'},
+		's1':{'DIGIT':'s1','.':'s3'},
+		's2':{'DIGIT':'s3'},
+		's3':{'DIGIT':'s3'}}
 
-     	      } 
 
-
-accepts = { 
 
 	# (Β) Συμπληρώστε το λεξικό των καταστάσεων αποδοχής και των
 	# αντίστοιχων επιστρεφόμενων συμβόλων (tokens)
 	# Για λεπτομέρειες δείτε στο:
 	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id8
-
-     	  }
+	accepts = {'s3':'FLOAT_TOKEN'}
 
 
 def get_char(text,pos):
@@ -39,7 +37,8 @@ def get_char(text,pos):
 	# χαρακτήρες εισόδου εδώ.
 	# Για λεπτομέρειες δείτε στο:
 	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id11
-	
+	if c>='0' and c<='9':
+		return 'DIGIT'
 	return c
 	
 
